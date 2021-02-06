5c637f7264d84040bea84495fda32bcf0e89ff2e
Vinit43 <68951357+Vinit43@users.noreply.github.com>
2021-02-06 20:02:54 +0530

    Add files via upload

---
+++ Numeric/Main.py
@@ -0,0 +1,18 @@
+
+
+No = 11
+print("\nValue of No is:",No,"\n")
+print("Data type of No is :",type(No),"\n")
+print("Unique ID of No is:",id(No),"\n")
+
+
+
+No2 = 11.11
+print("\nValue of No2 is:",No2,"\n")
+print("Data type of No2 is :",type(No2),"\n")
+print("Unique ID of No2 is:",id(No2),"\n")
+
+No3 = 4 + 3j
+print("\nValue of No3 is:",No3,"\n")
+print("Data type of No3 is :",type(No3),"\n")
+print("Unique ID of No3 is:",id(No3),"\n")
