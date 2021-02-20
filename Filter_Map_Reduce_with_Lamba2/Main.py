e84a7b667dfc97777b195bf820c0366014e13c72
Vinit43 <68951357+Vinit43@users.noreply.github.com>
2021-02-20 21:57:09 +0530

    Add files via upload

---
+++ Filter_Map_Reduce_with_Lamba2/Main.py
@@ -0,0 +1,42 @@
+# Accept N numbers from user and filter out only even number from that N number
+# after that add 2 in every even number
+# after that perform summation of all that modified number
+
+#Input : 1 2 3 4 5 
+#Ouput: 10
+import functools
+
+
+def main():
+
+	List=[]
+	N = int(input("Enter the value of N \n"))
+	
+	print("Enter the N numbers")
+	for i in range(0,N,1):
+		value=int(input())
+		List.append(value)
+		
+
+	print("The entered N numbers are:",List)
+
+
+	ret=list(filter(lambda no: (no%2==0),List))     #ret=Filterr(List)
+
+	print("The filtered list is:",ret)
+
+	
+	ret1=list(map(lambda no: no+2,ret))    #ret1=Map(ret)
+
+	print("The mapped list is:",ret1)
+
+	ret2=functools.reduce(lambda no1,no2:no1+no2,ret1)		#ret2=Reduce(ret1) need to import a specific module for Python3
+
+	print("The reduced output is:",ret2)
+
+
+
+if __name__ == "__main__":
+	main()
+
+# name = lambda parameters:body
---
+++ Filter_Map_Reduce_with_actualFunc/Main.py
@@ -0,0 +1,56 @@
+# Accept N numbers from user and filter out only even number from that N number
+# after that add 2 in every even number
+# after that perform summation of all that modified number
+
+#Input : 1 2 3 4 5 
+#Ouput: 10
+import functools
+
+def CheckEven(no):
+
+	if no%2==0:
+		return True
+	else:
+		return False
+
+def Increment(no):
+
+	return no+2
+
+def Addition(no1,no2):
+
+	return no1+no2 
+
+def main():
+
+	List=[]
+	N = int(input("Enter the value of N \n"))
+	
+	print("Enter the N numbers")
+	for i in range(0,N,1):
+		value=int(input())
+		List.append(value)
+		
+
+	print("The entered N numbers are:",List)
+
+
+	ret=list(filter(CheckEven,List))     #ret=Filterr(List)
+
+	print("The filtered list is:",ret)
+
+	
+	ret1=list(map(Increment,ret))    #ret1=Map(ret)
+
+	print("The mapped list is:",ret1)
+
+	ret2=functools.reduce(Addition,ret1)		#ret2=Reduce(ret1) need to import a specific module for Python3
+
+	print("The reduced output is:",ret2)
+
+
+
+if __name__ == "__main__":
+	main()
+
+# name = lambda parameters:body
